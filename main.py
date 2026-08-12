a863a1b8096ce158b4c1b5b875d614bb8bff5a75
nashgouren-hue <nashgouren@gmail.com>
2026-08-12 16:33:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -65,11 +65,10 @@ async def create_welcome_image(member):
 
     nama = member.display_name
 
-    try:
-        font_nama = ImageFont.truetype("arialbd.ttf", 400)
-    except:
-        font_nama = ImageFont.load_default()
-
+   try:
+    font_nama = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 100)
+except:
+    font_nama = ImageFont.load_default()
     bbox = draw.textbbox(
         (0, 0),
         nama,
