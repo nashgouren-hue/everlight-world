982b5314d37c6fc7627cb0d4b468f709f5426469
nashgouren-hue <nashgouren@gmail.com>
2026-08-12 16:25:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ async def create_welcome_image(member):
     nama = member.display_name
 
     try:
-        font_nama = ImageFont.truetype("arialbd.ttf", 100)
+        font_nama = ImageFont.truetype("arialbd.ttf", 250)
     except:
         font_nama = ImageFont.load_default()
 
