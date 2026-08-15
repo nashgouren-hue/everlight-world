6054027d4e0f6dea953e3d595ca6b9005b0b08b5
nashgouren-hue <nashgouren@gmail.com>
2026-08-15 15:18:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -701,7 +701,8 @@ async def start_web_server():
         return web.Response(text="TikTok callback ready")
         
     app.router.add_get("/test", test_route)
-
+    app.router.add_get("/cek-tiktok", verify_root_txt)
+    
     app.router.add_get(
         "/tiktokIw0KJ6SVO5YSdgbS2vKFqeTW40mKZ25P.txt",
         verify_root_txt
